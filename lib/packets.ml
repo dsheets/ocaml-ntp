322cbef257cad567676e225d3f6bd8efa702708b
Gabriel McArthur <gabriel.mcarthur@gmail.com>
2014-09-23 13:52:54 -1000

    Yet more failure.

--- lib/packets.ml
+++ lib/packets.ml
@@ -4,17 +4,23 @@
 
 (* RFC5905 *)
 
-open Printf
-open Operators
-open Name
 open Cstruct
 
 (* Local clock process return codes *)
-cenum local_clock {
+cenum local_clock_action {
   IGNORE = 0; (* ignore *)
   SLEW = 1;   (* slew adjustment *)
   STEP = 2;   (* step adjustment *)
   PANIC = 3   (* panic - no adjustment *)
 } as uint8_t
 
+(* System flags *)
+cenum system_flags {
+  FLAGS = 0;  (* any system flags *)
+  BCSTENAB = 0x1; (* enable broadcast client *)
+}
 
+(* Peer flags *)
+cenum peer_flags {
+  FLAGS = 0;
+}
